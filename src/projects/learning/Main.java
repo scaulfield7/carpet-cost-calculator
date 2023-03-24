66ec3b8c31e103f760af1b243e060890c5f61d24
scaulfield7 <scaulfield@live.ie>
2023-03-24 14:28:07 +0000

    Add case where one value is less than 0

--- src/projects/learning/Main.java
+++ src/projects/learning/Main.java
@@ -2,10 +2,15 @@ package projects.learning;
 
 public class Main {
     public static void main(String[] args) {
-        Floor floor = new Floor(12.4, 16.8);
-        Carpet carpet= new Carpet(32);
-        CostCalculator costCalculator = new CostCalculator(floor, carpet);
+        Floor floor1 = new Floor(12.4, 16.8);
+        Carpet carpet1= new Carpet(32);
+        CostCalculator costCalculator1 = new CostCalculator(floor1, carpet1);
 
-        System.out.println("Cost of floor: " + costCalculator.getTotalCost());
+        Floor floor2 = new Floor(4.1, -13);
+        Carpet carpet2= new Carpet(24);
+        CostCalculator costCalculator2 = new CostCalculator(floor2, carpet2);
+
+        System.out.println("Cost of floor 1: " + costCalculator1.getTotalCost());
+        System.out.println("Cost of floor 2: " + costCalculator2.getTotalCost());
     }
 }
