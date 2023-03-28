6b63468b0ddb9c035c4d159b8f18a099a5d494fc
scaulfield7 <scaulfield@live.ie>
2023-03-28 11:25:04 +0100

    Format decimals to 2 decimal places

--- src/projects/learning/Main.java
+++ src/projects/learning/Main.java
@@ -1,7 +1,10 @@
 package projects.learning;
 
+import java.text.DecimalFormat;
+
 public class Main {
     public static void main(String[] args) {
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
         Floor floor1 = new Floor(12.4, 16.8);
         Carpet carpet1= new Carpet(32);
         CostCalculator costCalculator1 = new CostCalculator(floor1, carpet1);
@@ -10,7 +13,7 @@ public class Main {
         Carpet carpet2= new Carpet(24);
         CostCalculator costCalculator2 = new CostCalculator(floor2, carpet2);
 
-        System.out.println("Cost of floor 1: €" + costCalculator1.getTotalCost());
-        System.out.println("Cost of floor 2: €" + costCalculator2.getTotalCost());
+        System.out.println("Cost of floor 1: €" + decimalFormat.format(costCalculator1.getTotalCost()));
+        System.out.println("Cost of floor 2: €" + decimalFormat.format(costCalculator2.getTotalCost()));
     }
 }
